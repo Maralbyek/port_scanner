981222df8eac20091cb699852a400b9f7e1531fc
Maralbyek <maralbektilek@gmail.com>
2025-11-14 03:41:05 -0500

    Initial commit - port scanner Mini NMAP

---
+++ scanner.py
@@ -0,0 +1,21 @@
+import socket
+
+print("=== Beginner Port Scanner ===")
+
+target = input("Enter target IP or domain: ")
+
+for port in range(1, 101):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(0.5)
+
+    result = sock.connect_ex((target, port))
+
+    if result == 0:
+        print(f"[+] Port {port} is OPEN")
+    else:
+        print(f"[-] Port {port} is closed")
+
+    sock.close()
+
+print("Done!")
+
